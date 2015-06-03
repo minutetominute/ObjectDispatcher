76d215b4e3840ede2d223c78471f3cfc25df6ca7
Jose Garza <minutetominute@gmail.com>
2015-06-03 13:09:31 -0400

    minor style tweaks

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,7 +1,6 @@
-require_relative '02_searchable'
+require_relative 'searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -35,7 +34,6 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
     assoc_options[name] = options
@@ -60,6 +58,37 @@ module Associatable
   def assoc_options
     @assoc_options ||= Hash.new
   end
+
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options
+        .model_class.assoc_options[source_name]
+
+      source_table = source_options.table_name
+      through_table = through_options.table_name
+      primary_key = source_options.primary_key
+      through_foreign_key = through_options.foreign_key
+      source_foreign_key = source_options.foreign_key
+
+      rows = DBConnection.execute(<<-SQL, send(through_foreign_key))
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+          ON #{through_table}.#{source_foreign_key} = #{source_table}.#{primary_key}
+        WHERE
+          #{through_table}.id = ?
+      SQL
+
+      rows.map do |hash|
+        source_options.model_class.new(hash)
+      end.first
+    end
+  end
+
 end
 
 class SQLObject
